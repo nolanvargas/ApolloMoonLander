fd6fc565ff54e1ef72ac87e3ddb28a00d94b2115
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2023-01-28 15:38:51 -0700

    new  updatePosition method

--- lander.cpp
+++ lander.cpp
@@ -1,1 +1,81 @@
 #include "lander.h"
+#include "point.h"
+#include "thrust.h"
+#include "processKinematics.h"
+#include <tuple>
+
+ProcessKinematics pk;
+
+void Lander::reset() {
+	status = 0;
+}
+
+bool Lander::isDead() {
+	return status;
+}
+
+bool Lander::isFlying() {
+	return 1;
+}
+
+bool Lander::isLanded() {
+	return 1;
+}
+
+Point Lander::getPosition() {
+	return pt;
+}
+
+int Lander::getFuel() {
+	return fuel;
+}
+
+double Lander::getAngle()
+{
+	return angle;
+}
+
+void Lander::coast() {
+	pt.addY(-1.0);
+}
+
+void Lander::land() {
+	//I dont know
+}
+void Lander::crash() {
+	//boom
+}
+
+void Lander::input(int x) {
+	if (x == 4) {
+		//cout << pt.getX() << endl;
+		//cout << pt.getY() << endl;
+		double _x, _y;
+		std::tie( _x, _y) = pk.applyThrust(thrust, weight, v.getDx(), v.getDy(), angle);
+		v.setDx(_x);
+		v.setDy(_y);
+	}
+	else if (x == 4) {
+		pt.addY(-1.0);
+	}
+	if (x == 2) {
+		angle += 0.1;
+	}
+	else if (x == 1) {
+		angle -= 0.1;
+	}
+	
+}
+
+void Lander::updatePosition() {
+	//cout << pt.getX() << endl;
+	//cout << pt.getY() << endl;
+	//cout << v.getDx() << endl;
+	//cout << v.getDy() << endl;
+	v.setDy(pk.applyGravity(v.getDy(), (g / 10)));
+	double _x, _y;
+	tie(_x, _y) = pk.applyInertia(pt.getX(), pt.getY(), v.getDx(), v.getDy(), g);
+	pt.setX(_x);
+	pt.setY(_y);
+
+}
--- lander.h
+++ lander.h
@@ -6,31 +6,38 @@
 class Lander
 {
 private:
-	int status;
+	int status; //1 for dead 0 for alive
 	Point pt;
 	Velocity v;
-	double angle;
-	Point ptUpperRight;
-	int fuel; //Makes sense to make it an integer
-	double weight;
-	double thrust;
-	double g; //Gravity
+	double angle = 0;
+	Point ptUpperRight; // Size of the screen
+	int fuel = 5000; //Makes sense to make it an integer
+	const double weight = 15103.00;
+	const double thrust = 45000.00;
+	const double g = -1.625; //Gravity
 
 public:
 
 	// Constructor
-	Lander(Point ptUpperRight);
+	Lander(Point location, Point screen) : 
+		pt(location), 
+		ptUpperRight(screen)
+	{
+		status = 0;
+	};
 	void reset();
 	bool isDead();
 	bool isFlying();
 	bool isLanded();
 	Point getPosition();
+	double getAngle();
 	int getFuel();
-	void draw(thrust, gout);
-	void input(thrust);
+	//void draw(thrust, gout);
+	void input(int thrustHere);
 	void coast();
 	void land();
 	void crash();
+	void updatePosition();
 
 };
 
