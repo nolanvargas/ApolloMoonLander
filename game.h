c5e870218210a9aea817fbe86fbb4be2052bd87f
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2023-01-28 15:40:51 -0700

    updated includes

--- game.h
+++ game.h
@@ -1,24 +1,29 @@
-//#pragma once
-//#include 'lander.h'
-//#include 'point.h'
-//
-//class Game
-//{
-//private:
-//	Lander lander();
-//	Point ptUpperRight();
-//	Ground ground();
-//	Stars stars[];
-//
-//public:
-//		
-//	// Contructor
-//	Game(ptUpperRight);
-//
-//	void reset();
-//	void input(ui);
-//	void gamePlay(thrust);
-//	void display(thrust);
-//
-//};
+#pragma once
+#include "lander.h"
+#include "point.h"
+#include "ground.h"
+#include "star.h"
+#include "thrust.h"
+#include <vector> // This may need to be changed
+
+class Game
+{
+private:
+	Lander lander;
+	Point ptUpperRight;
+	Ground ground;
+	Thrust thrust;
+	std::vector<Star> stars; // Vector of star objects
+
+public:
+		
+	// Contructor
+	Game(Point ptUpperRight);
+
+	void reset();
+	void input(/*ui*/); //ui draw maybe?
+	void gamePlay(Thrust thrust);
+	void display(Thrust thrust);
+
+};
 
