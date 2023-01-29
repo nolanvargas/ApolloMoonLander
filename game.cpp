952cce86a0bbd07bd59e3b5518c971b8b9d5710c
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2023-01-28 20:32:51 -0700

    Standaline functionality added

--- game.cpp
+++ game.cpp
@@ -1,1 +1,46 @@
+#pragma once
+#include "uiInteract.h"
+#include "uiDraw.h"
+#include "point.h"
+#include "ground.h"
+#include "lander.h"
+#include "star.h"
+#include "thrust.h"
+#include <vector> // This may need to be changed
 #include "game.h"
+
+using namespace std;
+
+void Game::input(const Interface* pUI) {
+    // move the ship around
+    if (pUI->isRight())
+        mL.input(1);
+    if (pUI->isLeft())
+        mL.input(2);
+    if (pUI->isUp())
+        mL.input(3);
+    if (pUI->isDown())
+        mL.input(4);
+}
+
+void Game::gamePlay(Thrust thrust) {
+    mL.updatePosition();
+}
+
+void Game::display(Thrust thrust, const Interface* pUI) {
+    ogstream gout;
+
+    ground.draw(gout);
+
+    // draw the lander and its flames
+    gout.drawLander(mL.getPosition() /*position*/, mL.getAngle() /*angle*/);
+    gout.drawLanderFlames(mL.getPosition(), mL.getAngle(), /*angle*/
+        pUI->isDown(), pUI->isLeft(), pUI->isRight());
+
+    // draw our little star
+    gout.drawStar(ptStar, phase++);
+
+    // draw the lander stats
+    gout.setPosition(Point(30.0, 30.0));
+    gout << "Fuel:\t" << mL.getFuel() << "\nAltitude:\t" << mL.getPosition().getY() << "\nSpeed:\t" << "0000";
+}
--- game.h
+++ game.h
@@ -1,7 +1,9 @@
 #pragma once
-#include "lander.h"
+#include "uiInteract.h"
+#include "uiDraw.h"
 #include "point.h"
 #include "ground.h"
+#include "lander.h"
 #include "star.h"
 #include "thrust.h"
 #include <vector> // This may need to be changed
@@ -9,21 +11,32 @@
 class Game
 {
 private:
-	Lander lander;
-	Point ptUpperRight;
+	Point ptLM;           // location of the LM on the screen
+	Point ptUpperRight;   // size of the screen
+	unsigned char phase;  // phase of the star's blinking
 	Ground ground;
+	Point ptStar;
+	Lander mL;
+
 	Thrust thrust;
 	std::vector<Star> stars; // Vector of star objects
 
 public:
 		
 	// Contructor
-	Game(Point ptUpperRight);
+	Game(const Point& ptUpperRight) :
+		ptStar(ptUpperRight.getX() - 20.0, ptUpperRight.getY() - 20.0),
+		ptLM(ptUpperRight.getX() / 2.0, ptUpperRight.getY() / 2.0),
+		ground(ptUpperRight), mL(Point(250.0, 250.0), ptUpperRight)
+	{
+
+		phase = random(0, 255);
+	};
 
 	void reset();
-	void input(/*ui*/); //ui draw maybe?
+	void input(const Interface* pUI); //ui draw maybe?
 	void gamePlay(Thrust thrust);
-	void display(Thrust thrust);
+	void display(Thrust thrust, const Interface* pUI);
 
 };
 
